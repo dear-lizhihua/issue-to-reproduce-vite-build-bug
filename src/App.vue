cac9229ee23ad359983601d15788acacaeb7697b
lizhihua <lizhihua@qianxin.com>
2021-05-30 12:12:03 +0800

    feat: update

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  {{text}}
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 })
+
+import {Sitdown} from 'sitdown'
+
+const text = (new Sitdown()).HTMLToMD('<h1>Hello World</h1>')
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
