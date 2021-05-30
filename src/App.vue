09d69caf4d81bd8c0d069aedd1814a04b2fabcd9
lizhihua <lizhihua@qianxin.com>
2021-05-30 12:13:24 +0800

    feat: update

--- src/App.vue
+++ src/App.vue
@@ -14,18 +14,4 @@ const state = reactive({ count: 0 })
 import {Sitdown} from 'sitdown'
 
 const text = (new Sitdown()).HTMLToMD('<h1>Hello World</h1>')
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
